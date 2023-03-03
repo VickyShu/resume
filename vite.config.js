38666f0256cce93b497d27a3d423649ed614ea32
舒欣 <shuxin@baqi.lan>
2023-03-03 00:29:46 -0800

    :tada: 初始化项目.

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    // 设置项目根目录
+    base: './',
+    server: {
+        port: 5001
+    },
+    plugins: [
+        vue(
+            {
+                // 开启响应式语法糖, 用来支持$ref()
+                reactivityTransform: true
+            }
+        ),
+        AutoImport({
+            import: ['vue'],
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    prefix: 'Icon',
+                })
+            ],
+        }),
+        Components({
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    enabledCollections: ['ep'],
+                })
+            ],
+        }),
+        Icons({
+            autoInstall: true,
+        }),
+    ],
+})
